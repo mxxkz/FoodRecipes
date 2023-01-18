2d61cf74b3b1b6c98b2eec45ce0ff3c326d4425e
mxxkz <jbmook@outlook.co.th>
2023-01-18 10:37:22 +0700

    Edit path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ function Header() {
     <div className="w-full mb-10 relative">
       <img
         className="object-cover w-full h-96 contrast-75 brightness-50"
-        src="/pic/food.JPG"
+        src="pic/food.JPG"
+        alt=""
       />
       <p className="absolute text-7xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-black z-30 drop-shadow-lg shadow-amber-400 text-neutral-100">
         Good Food Good Life
